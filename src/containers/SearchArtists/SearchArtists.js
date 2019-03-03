573769a064b01bfb342730baa922611c504eb473
fcalvo <fernando.calvo.sanz@gmail.com>
2019-03-03 14:11:29 +0000

    Addd searchArtists container

---
+++ src/containers/SearchArtists/SearchArtists.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import SearchBox from '../../components/SearchBox';
+import ArtistBox from '../../components/ArtistBox';
+import './styles.css';
+
+const SearchArtists = ({ findArtists, artists }) => {
+	return (
+		<div className="search-artists">
+			<SearchBox onInput={text => findArtists(text)}/>
+			<div className="artists-results">
+				{artists && artists.map(artist => (
+					<ArtistBox artist={artist} />
+				))}
+			</div>
+		</div>
+	)
+}
+
+const mapState = ({ artists }) => ({
+	artists
+})
+
+const mapDispatch = ({ artists }) => ({
+	findArtists: artists.findArtists,
+})
+
+export default connect(mapState, mapDispatch)(SearchArtists);

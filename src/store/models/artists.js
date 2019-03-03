2b86c9b973e4b6764f22d409ca9b183896ed2342
fcalvo <fernando.calvo.sanz@gmail.com>
2019-03-03 13:51:06 +0000

    Add artists Redux model

--- src/store/models/artists.js
+++ src/store/models/artists.js
@@ -1,6 +1,31 @@
+import uuid from 'uuid/v1';
+import axios from 'axios';
 export default {
   state: [],
   reducers: {
-    reset: () => ({})
-  }
+    addArtist(state, name, image) {
+      return [
+        ...state,
+        {
+          id: uuid(),
+          name,
+          image,
+        }
+      ];
+    },
+    reset() {
+      return [];
+    }
+  },
+  effects: (dispatch) => ({
+    async findArtists(text) {
+      const payload = {
+        params: `query=${text}&hitsPerPage=6&filters=type%3Aartists`
+      }
+      const { data } = await axios.post('https://ufhsub9629-dsn.algolia.net/1/indexes/search/query?x-algolia-application-id=UFHSUB9629&x-algolia-api-key=69ed687a250f4c895cc73f6ee142a42e', JSON.stringify(payload));
+      data.hits.forEach(artistData => {
+        dispatch.artists.addArtist(artistData.title_en, artistData.img)
+      })
+    }
+  }),
 };
